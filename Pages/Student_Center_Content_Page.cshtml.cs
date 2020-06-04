75ef11b8b2ee946bbf045a59c9f9b5fc70d65c16
AJ <pfunda@seattleu.edu>
2020-06-03 22:04:40 -0700

    * Student_Center_Content_Page.cshtml: Re-Add cshtml   Student_Center_Content_Page to AsparageTours
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/q2a58LJvDkagn6hYhKa2emQAC7BC?Type=TaskLink&Channel=Link&CreatedTime=637268438678790000
    
    * Student_Center_Content_Page.cshtml.cs: Re-Add cshtml.cs
      Student_Center_Content_Page to AsparageTours
    
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/q2a58LJvDkagn6hYhKa2emQAC7BC?Type=TaskLink&Channel=Link&CreatedTime=637268438678790000

---
+++ Pages/Student_Center_Content_Page.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AsparageTours.Pages
+{
+    public class Student_Center_Content_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
