ae4322a875737e7a1c61f7f43b6851139909d28c
messnerd <messnera@seattleu.edu>
2020-06-04 12:11:57 -0700

    added page gym_favorite to Pages folder https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/Rz2wcgFkGkmQODPeqp64C2QAG5cJ?Type=TaskLink&Channel=Link&CreatedTime=637268947124050000

---
+++ Pages/gym_favorite.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AsparageTours.Pages
+{
+    public class gym_favoriteModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
